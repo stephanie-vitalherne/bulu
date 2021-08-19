99eaae9fd1cbe38ca585fce683d47ab19d2e739a
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-08-19 13:12:09 -0400

    results done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
-import { Layout, Nav } from "../components";
+import { Layout, Nav, Results } from "../components";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <Layout title="Bulu">
       <Nav />
+      <Results results={results} />
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((response) => response.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
